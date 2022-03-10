88d4895d3beb1a69ef780a20977f6e89bc1193fa
Yolanda Ornellas <72880963+yornellas@users.noreply.github.com>
2022-03-10 10:22:32 -0300

    Add files via upload

---
+++ app/models/article.rb
@@ -0,0 +1,14 @@
+class Article < ActiveRecord::Base
+  has_many :comments, dependent: :destroy
+  
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
+end
---
+++ app/models/comment.rb
@@ -0,0 +1,11 @@
+class Comment < ActiveRecord::Base
+  belongs_to :article
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
+end
